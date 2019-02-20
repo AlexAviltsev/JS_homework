e4a997f29ca5b22b68bb737f32f36114333e6dbc
Alex <aaviltsev55@gmail.com>
2019-02-20 16:54:21 +0200

    ДЗ_2

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', () => {
 		confirm = document.querySelector('.confirm'),
 		badge = document.querySelector('.nav__badge'),
 		totalCost = document.querySelector('.cart__total > span'),
-		titles = document.querySelector('.goods__title');
+		titles = document.querySelectorAll('.goods__title');
 
 	function openCart() {
 		cart.style.display = 'block';
@@ -32,17 +32,86 @@ window.addEventListener('DOMContentLoaded', () => {
 				empty = cartWrapper.querySelector('.empty');
 
 			trigger.remove();
-		
+
+			showConfirm();
+			calcGoods(1);
+
 			removeBtn.classList.add('goods__item-remove')
 			removeBtn.innerHTML = '&times';
 			item.appendChild(removeBtn);
 
 			cartWrapper.appendChild(item);	
 			if (empty) {
-				empty.remove();
-			}
+				empty.style.display = 'none';
+			} 
+
+			calcTotal();
+			removeFromCart();
+			if (empty) {
+				empty.style.display = 'none';
+			} 
 		});
 	});
+
+	function sliceTitie() {
+		titles.forEach(function(item) {
+			if (item.textContent.length < 70) {
+				return;
+			} else {
+				const str = item.textContent.slice(0, 71) + '...';
+				// const str = `${item.textContent.slice(0, 71)}...`;
+				item.textContent = str;
+			}
+
+		});
+	}
+	sliceTitie();
+
+	function showConfirm() {
+		confirm.style.display = 'block';
+		let counter = 100;
+		const id = setInterval(frame, 10);	
+		function frame() {
+			if (counter == 10) {
+				clearInterval(id);
+				confirm.style.display = 'none';
+			} else {
+				counter--;
+				confirm.style.opacity = '.' + counter;
+				confirm.style.transform = `translateY(-${counter}px)`;
+			}	
+		
+		}
+	}
+
+	function calcGoods(i) {
+		const items = cartWrapper.querySelectorAll('.goods__item');
+		let empty = cartWrapper.querySelector('.empty');
+		badge.textContent = i + items.length;
+		if (badge.textContent < 1) {
+			empty.style.display = 'block';   //Домашнее задание 2!!!
+		}
+	}
+
+	function calcTotal() {
+		const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+		let total = 0;
+		prices.forEach(function(item) {
+			total += +item.textContent;	
+		});
+		totalCost.textContent = total;
+	}
+
+	function removeFromCart() {
+		const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+		removeBtn.forEach(function(btn) {
+			btn.addEventListener('click', () => {
+				btn.parentElement.remove();
+				calcGoods(0);
+				calcTotal();
+			});
+		});
+	}
 });
 
 
